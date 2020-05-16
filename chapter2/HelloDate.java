14aa1eab29d7d187f786f13c63b5840ea51cdac8
1941161938@qq.com <1941161938@qq.com>
2020-05-17 01:05:56 +0800

    java day1

---
+++ chapter2/HelloDate.java
@@ -0,0 +1,23 @@
+//: chapter2/HelloDate.java
+package chapter2;
+import java.util.*;
+
+/**
+ * The first Thinking in java example program.
+ * Displays a string and today's date.
+ * @author 19411
+ * @version 1.0
+ */
+public class HelloDate {
+    /**
+     * Entry point to class & application.
+     *
+     * @param args array of string arguments
+     * @throws exceptions No exceptions thrown
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello,it's: ");
+        System.out.println(new Date());
+    }
+}/* output:
+*///"~
---
+++ chapter2/helllo.java
@@ -0,0 +1,7 @@
+package chapter2;
+
+public class helllo {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
