d6570188382c67ec4d8ec08307874473ef212edc
1941161938@qq.com <1941161938@qq.com>
2020-06-15 16:28:54 +0800

    模板方法模式

---
+++ DesignPattern/templatemethod/sort/Duck.java
@@ -0,0 +1,31 @@
+package DesignPattern.templatemethod.sort;
+
+import java.util.Comparator;
+
+public class Duck implements Comparable<Duck> {
+    String name;
+    int weight;
+
+    public Duck(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + " weighs " + weight;
+    }
+
+    @Override
+    public int compareTo(Duck object) {
+        Duck otherDuck = object;
+
+        if (this.weight < otherDuck.weight) {
+            return -1;
+        } else if (this.weight == otherDuck.weight) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
