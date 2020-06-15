a56585c0394c33568d0b70c216ad6c329800394c
1941161938@qq.com <1941161938@qq.com>
2020-06-15 23:47:38 +0800

    代理模式——没看懂

---
+++ DesignPattern/proxy/gumball/State.java
@@ -0,0 +1,13 @@
+package DesignPattern.proxy.gumball;
+
+import java.io.Serializable;
+
+public interface State extends Serializable {
+    void insertQuarter();
+
+    void ejectQuarter();
+
+    void turnCrank();
+
+    void dispense();
+}
---
+++ DesignPattern/proxy/javaproxy/PersonBean.java
@@ -0,0 +1,23 @@
+package DesignPattern.proxy.javaproxy;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+
+public interface PersonBean {
+    String getName();
+
+    String getGender();
+
+    String getInterests();
+
+    int getHotOrNotRating();
+
+    void setName(String name);
+
+    void setGender(String gender);
+
+    void setInterests(String interests);
+
+    void setHotOrNotRating(int rating);
+}
