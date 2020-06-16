d47650b5503f4c7c3e6c5964eac3cbc5ab7b999c
1941161938@qq.com <1941161938@qq.com>
2020-06-16 14:41:09 +0800

    builder模式

---
+++ DesignPattern/builder/ConcreteBuilder.java
@@ -0,0 +1,15 @@
+package DesignPattern.builder;
+
+public class ConcreteBuilder extends Builder {
+    public void buildPartA() {
+        product.setPartA("建造 PartA");
+    }
+
+    public void buildPartB() {
+        product.setPartA("建造 PartB");
+    }
+
+    public void buildPartC() {
+        product.setPartA("建造 PartC");
+    }
+}
